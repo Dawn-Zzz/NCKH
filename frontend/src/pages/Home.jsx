97f5f6d17a9f12e28be6b26c44e33df2f1f5a075
3Luan <nguyenthanhluanbntsbd@gmail.com>
2024-02-26 10:03:38 +0700

    'fix'

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+const Home = () => {
+  return <>Home</>;
+};
+
+export default Home;
